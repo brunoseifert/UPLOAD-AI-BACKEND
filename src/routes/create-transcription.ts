32f262eee40f4b557d403aa9c891ba1456d4b385
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2023-09-14 16:21:45 -0300

    api-openai

--- src/routes/create-transcription.ts
+++ src/routes/create-transcription.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from "fastify";
+import { createReadStream } from "node:fs";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
-import { createReadStream } from "node:fs";
+import { openai } from "../lib/openai";
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
   app.post("/videos/:videoId/transcription", async (req) => {
@@ -24,11 +25,30 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
     });
 
     const videoPath = video.path;
+    const audioReadStream = createReadStream(videoPath);
+
+    const response = await openai.audio.transcriptions.create({
+      file: audioReadStream,
+      model: "whisper-1",
+      language: "pt",
+      response_format: "json",
+      temperature: 0,
+      prompt,
+    });
+
+    const transcription = response.text;
+
+    await prisma.video.update({
+      where: {
+        id: videoId,
+      },
+      data: {
+        transcription,
+      },
+    });
 
     return {
-      videoId,
-      prompt,
-      videoPath,
+      transcription,
     };
   });
 }
